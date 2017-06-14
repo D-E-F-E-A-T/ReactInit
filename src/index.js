867953689db9cc9f0685669e0ba1d7766102b6b0
Evgeniya Bautina <janebarinova@gmail.com>
2017-06-14 12:29:22 -0700

    Add: Style1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ var ReactDOM = require('react-dom');
 //var App = React.createElement('h1', null, "Hello World");
 //var App = <h1> Hello World </h1>;// JSX
 var Component = function(props) {
+  var style = {
+    color: props.color
+  };
   return (
-    <h1>{props.greeting}</h1>
+    <div style={style}>
+      <h1>{props.greeting}</h1>
+    </div>
   );
 }
 ReactDOM.render(
-  <Component greeting="Hello World" />,
+  <Component greeting="Hello World" color="green" />,
   document.getElementById('root')
 );
