af3959b54dac802e936bf2a22f388584667d70c2
Evgeniya Bautina <janebarinova@gmail.com>
2017-06-14 12:17:55 -0700

    Add: component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var App = React.createElement('h1', null, "Hello World");
+//var App = React.createElement('h1', null, "Hello World");
+//var App = <h1> Hello World </h1>;// JSX
+var Component = function() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
 ReactDOM.render(
-  App,
+  <Component/>,
   document.getElementById('root')
 );
