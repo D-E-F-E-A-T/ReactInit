154e46ac1720d5a1bd429d01a0c1c85fe507d924
Evgeniya Bautina <janebarinova@gmail.com>
2017-06-14 12:03:22 -0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = React.createElement('h1', null, "Hello World");
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
