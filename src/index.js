50ee0d6f1ba1bf895690853a4cfe78819a3dba56
Evgeniya Bautina <janebarinova@gmail.com>
2017-06-14 13:02:00 -0700

    Edit: README

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var createClass = require('create-react-class');
-/*var Component = function(props) {
-  return (
-    <div style={{color:props.color}}>
-      <h1>{props.greeting}</h1>
-    </div>
-  );
-}*/
 var Component = createClass({
   render: function() {
     return(
