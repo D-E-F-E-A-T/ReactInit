815ce9136f5454e68f71d44f200bb5698428e5ad
Evgeniya Bautina <janebarinova@gmail.com>
2017-06-14 12:59:52 -0700

    Add: Class Component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-//var App = React.createElement('h1', null, "Hello World");
-//var App = <h1> Hello World </h1>;// JSX
-var Component = function(props) {
+var createClass = require('create-react-class');
+/*var Component = function(props) {
   return (
     <div style={{color:props.color}}>
       <h1>{props.greeting}</h1>
     </div>
   );
-}
+}*/
+var Component = createClass({
+  render: function() {
+    return(
+      <div style={{color:this.props.color}}>
+        <h1>{this.props.greeting}</h1>
+      </div>
+    );
+  }
+});
 ReactDOM.render(
   <div>
     <Component greeting="Hello World" color="blue" />
-    <Component greeting="Hey there" color="green" />
   </div>,
   document.getElementById('root')
 );
